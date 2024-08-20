13c1596797a4a43bc82aed2909741363f6f41a51
Himanshu singh <42588870+himanshuSinghworkPort@users.noreply.github.com>
2024-08-20 19:56:00 +0530

    Add files via upload

---
+++ Chatboat -Using Swing-AI/ChatBot.java
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @author HIMANSHU
+ * 
+ *
+ */
+public class ChatBot {
+
+	public static void main(String[] args) {
+		ChatBotMain obj=new ChatBotMain();
+		obj.setTitle("ChatBot(Team-SVINFOTECH)");
+		obj.setVisible(true);
+	}
+}
